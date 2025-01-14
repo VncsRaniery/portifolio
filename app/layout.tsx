0db6a8bd8a5efeb3674a82ae9c1e534b62bed787
Vinicius Raniery <152623369+VncsRaniery@users.noreply.github.com>
2025-01-14 11:51:21 -0300

    Initial Commit

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Portifolio - Vinícius Raniery",
+  description: "Portifolio feito por Vinícius Raniery para apresentar o seu trabalho.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
